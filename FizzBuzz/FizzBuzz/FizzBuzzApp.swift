d26522e32cb5b0ae34210e19614e60157a5ec092
abylay-kun <52811230+abylay-kun@users.noreply.github.com>
2023-05-21 09:55:39 +0600

    FizzBuzz game logic in swiftt

---
+++ FizzBuzz/FizzBuzz/FizzBuzzApp.swift
@@ -0,0 +1,17 @@
+//
+//  FizzBuzzApp.swift
+//  FizzBuzz
+//
+//  Created by Abylay Duzel on 17.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct FizzBuzzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
