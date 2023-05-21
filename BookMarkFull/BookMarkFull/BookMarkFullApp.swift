1aa49dbabf79c8cd7a7536a0859d1ceb2b8002de
abylay-kun <52811230+abylay-kun@users.noreply.github.com>
2023-05-21 09:46:31 +0600

    Bookmark App written in swiftUI using API

---
+++ BookMarkFull/BookMarkFull/BookMarkFullApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookMarkFullApp.swift
+//  BookMarkFull
+//
+//  Created by Abylay Duzel on 17.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct BookMarkFullApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
